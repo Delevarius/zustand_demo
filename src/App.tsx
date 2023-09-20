b91d3ef12fc0e86730ba183a3b073c53bace51ec
delevarius <delevarius@gmail.com>
2023-09-21 00:04:47 +0200

    wrap styling

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import TodoList from './components/TodoList';
 import Header from './components/Header';
 
 const App = () => (
-  <div className='w-full h-full flex items-center justify-center'>
-    <div className='w-full lg:w-[75%] flex flex-col space-y-10 p-20'>
+  <div className='w-full h-screen flex items-center justify-center bg-slate-200 p-10'>
+    <div className='w-full lg:w-[75%] h-full flex flex-col space-y-10 p-20 bg-white'>
       <Header />
       <TodoList />
     </div>
